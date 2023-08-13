15731ad3def3ecfa7e409fd5882441ca6ed454d4
Tatiana Plastinina <61475346+tplastinina@users.noreply.github.com>
2023-08-13 19:10:31 +0200

    Add files via upload

---
+++ 3.5.3.py
@@ -0,0 +1,13 @@
+import requests
+# i = input("число?" )
+with open("C:/Users/tanze/Documents/Python/datten/dataset_24476_3.txt") as f:
+    f_lines = f.read().splitlines() #прочитали и разделили на линии
+    for line in f_lines:
+        i = line
+        res = requests.get(f'http://numbersapi.com/{i}/math?json=true')
+        data = res.json()
+        template = 'Число {} {}'
+        if data['found'] == True:
+            print(template.format(line, 'Interesting'))
+        else:
+            print(template.format(line, 'Boring')) 
